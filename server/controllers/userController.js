2322ceeaf0e3420a8c6468c0c6eb510f3daa068b
Atharv Joshi <atharvj07@gmail.com>
2025-02-12 11:29:35 +0530

    backend complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const Course = require("../models/Course");
+const CourseProgress = require("../models/CourseProgress");
 const Purchase = require("../models/Purchase");
 const User = require("../models/User");
 const Stripe = require('stripe')
@@ -173,6 +174,100 @@ const purchaseCourse = async (req, res) => {
         });
     }
 };
+//update User Course Progress
+const updateUserCourseProgress = async(req,res)=>{
+    try{
+        const userId = req.auth.userId
+        const {courseId,lectureId} = req.body;
+        const progressData = await CourseProgress.findOne({userId,courseId})
+        if(progressData){
+            if(progressData.lectureCompleted.includes(lectureId)){
+                return res.status(400).json({success:false,message:"Lecture already completed"})
+            }
+            progressData.lectureCompleted.push(lectureId)
+            await progressData.save()
+        }else{
+            await CourseProgress.create({
+                userId,
+                courseId,
+                lectureCompleted:[lectureId]
 
-
-module.exports = {getUserData,getUserEnrolledCourses,purchaseCourse}
+                
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message:"progress Updateed"
+        })
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+//get User Course Progress
+const getUserCourseProgress = async(req,res)=>{
+    try{
+        const userId = req.auth.userId
+        const {courseId} = req.body;
+        const progressData = await CourseProgress.findOne({userId,courseId})
+        return res.status(200).json({
+            success:true,
+            progressData
+        })
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+// add User Ratings to Course
+const addUserRating = async (req,res)=>{
+    const userId = req.auth.userId
+    const {courseId,rating} = req.body;
+    if(!courseId || !userId || !rating < 1 || rating > 5  ){
+        return res.status(400).json({
+            success:false,
+            message:"Invalid Details"
+        })
+    }
+    try{
+        const course = await Course.findById(courseId);
+        if(!course){
+            return res.status(404).json({
+                success:false,
+                message:"Course Not Found"
+            })
+        }
+        const user = await User.findById(userId);
+        if(!user || !user.enrolledCourses.includes(courseId)){
+            return res.status(400).json({
+                success:false,
+                message:"User has Not Purchased the Course"
+            })
+        }
+        const existingRatingIndex = course.courseRatings.findIndex(r =>r.userId === userId)
+        if(existingRatingIndex > -1){
+            course.courseRatings[existingRatingIndex].rating = rating
+        }else{
+            course.courseRatings.push({userId,rating})
+        }
+        await course.save()
+        return res.status(200).json({
+            success:true,
+            message:"Rating Added Successfully"
+        })  
+       
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+module.exports = {getUserData,getUserEnrolledCourses,purchaseCourse,updateUserCourseProgress,getUserCourseProgress,addUserRating}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const { getUserData, getUserEnrolledCourses, purchaseCourse } = require('../controllers/userController')
+const { getUserData, getUserEnrolledCourses, purchaseCourse, updateUserCourseProgress, getUserCourseProgress, addUserRating } = require('../controllers/userController')
 
 const router = express.Router()
 
 router.get('/data',getUserData);
 router.get('/enrolled-courses',getUserEnrolledCourses)
 router.post('/purchase',purchaseCourse)
+router.post('/update-course-progress',updateUserCourseProgress)
+router.post('/get-course-progress',getUserCourseProgress)
+router.post('add-rating', addUserRating)
 module.exports = router;
