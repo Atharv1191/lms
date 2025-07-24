edf9e3309e3920adae2e523515cf61f7e160a261
Atharv Joshi <atharvj07@gmail.com>
2025-07-24 14:37:36 +0530

    fix: update stripe instance reference in purchaseCourse function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -75,7 +75,8 @@ const purchaseCourse = async (req, res) => {
       quantity: 1
     }];
 
-    const session = await stripeInstance.checkout.sessions.create({
+   const session = await stripe.checkout.sessions.create({
+
       success_url: `${origin}/loading/my-enrollments`,
       cancel_url: `${origin}/`,
       line_items,
